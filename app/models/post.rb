b5f08558b597b9da72e11ebb83f29c1048601ca4
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-06 15:09:34 -0400

    Pass all tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, exclusion: { in: ["True Facts"] }
+  validates :content, length: {:minimum => 250}
+  validates :summary, length: {:maximum => 250}
+  validates :category, inclusion: { in: ['Non-Fiction', 'Fiction'] }
 end
